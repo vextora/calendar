6a4897990b639cecaf470b649261a058ffcfbaa3
oncomtechno <oncom.techno@gmail.com>
2025-05-20 12:00:24 +0700

    update casbin without model.conf

--- pkg/accesscontrol/casbin/enforcer.go
+++ pkg/accesscontrol/casbin/enforcer.go
@@ -9,14 +9,36 @@ import (
 	"gorm.io/gorm"
 )
 
+const casbinModelText = `
+[request_definition]
+r = sub, obj, act
+
+[policy_definition]
+p = sub, obj, act
+
+[role_definition]
+g = _, _
+g2 = _, _
+
+[policy_effect]
+e = some(where (p.eft == allow))
+
+[matchers]
+m = g(r.sub, p.sub) && r.obj == p.obj && r.act == p.act || eval(r.sub.IsOwner == true)
+`
+
 func NewEnforcer(db *gorm.DB) (*casbin.Enforcer, error) {
 	adapter, err := gormadapter.NewAdapterByDB(db)
 	if err != nil {
 		return nil, err
 	}
 
-	modelPath := filepath.Join("pkg", "accesscontrol", "casbin", "model.conf")
-	enforcer, err := casbin.NewEnforcer(modelPath, adapter)
+	m, err := model.NewModelFromString(casbinModelText)
+	if err != nil {
+		return nil, err
+	}
+	
+	enforcer, err := casbin.NewEnforcer(m, adapter)
 	if err != nil {
 		return nil, err
 	}
